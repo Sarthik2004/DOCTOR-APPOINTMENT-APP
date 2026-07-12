59c08877fd1668b95d26ab78276da588ff522d17
Sarthik <sarthikk2004@gmail.com>
2026-07-12 16:24:28 +0530

    Implemented complete doctor CRUD

--- backend/controllers/doctorController.js
+++ backend/controllers/doctorController.js
@@ -39,4 +39,82 @@ export const getDoctors = async (req, res) => {
   }
 };
 
+// Get Single Doctor
+export const getDoctorById = async (req, res) => {
+  try {
+    const { id } = req.params;
 
+    const doctor = await Doctor.findById(id);
+
+    if (!doctor) {
+      return res.status(404).json({
+        success: false,
+        message: "Doctor not found",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      doctor,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+export const updateDoctor = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const doctor = await Doctor.findByIdAndUpdate(id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!doctor) {
+      return res.status(404).json({
+        success: false,
+        message: "Doctor not found",
+      });
+    }
+    res.status(200).json({
+      success: true,
+      message: "Doctor Updated Successfully",
+      doctor,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+// Delete Doctor
+export const deleteDoctor = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const doctor = await Doctor.findById(id);
+
+    if (!doctor) {
+      return res.status(404).json({
+        success: false,
+        message: "Doctor not found",
+      });
+    }
+
+    await doctor.deleteOne();
+
+    res.status(200).json({
+      success: true,
+      message: "Doctor Deleted Successfully",
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
--- backend/routes/doctorRoutes.js
+++ backend/routes/doctorRoutes.js
@@ -1,10 +1,19 @@
 import express from "express";
-import { addDoctor, getDoctors } from "../controllers/doctorController.js";
+import {
+  addDoctor,
+  deleteDoctor,
+  getDoctorById,
+  getDoctors,
+  updateDoctor,
+} from "../controllers/doctorController.js";
 import { isAdmin, protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/", addDoctor, protect, isAdmin);
 router.get("/", getDoctors);
+router.get("/:id", getDoctorById);
+router.put("/:id", protect, isAdmin, updateDoctor);
+router.delete("/:id", protect, isAdmin, deleteDoctor);
 
 export default router;
