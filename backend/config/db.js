63d81e26e939e1a7e0d41103f7884966e535edca
Sarthik <sarthikk2004@gmail.com>
2026-07-14 12:41:40 +0530

    update config

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_url);
+    await mongoose.connect(process.env.MONGO_URL);
     console.log("MongoDb Connected Successfully");
   } catch (error) {
     console.log(error.message);
